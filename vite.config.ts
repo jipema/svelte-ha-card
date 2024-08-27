3bb9ed6f00cfc5c6bc30993b7eba1838b1e70ff5
Jean-Philippe MATZ <jeanphilippe.matz@gmail.com>
2024-08-27 23:46:55 +0200

    fix lib build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-// https://vitejs.dev/config/
+const IS_DEV = process.env.IS_DEV === "true";
+
 export default defineConfig({
   build: {
+    sourcemap: IS_DEV,
+    lib: IS_DEV
+      ? undefined
+      : {
+          entry: "./src/lib/index.ts",
+          name: "svelte-ha-card",
+        },
     rollupOptions: {
       output: {
         entryFileNames: `svelte-ha-card.js`,
